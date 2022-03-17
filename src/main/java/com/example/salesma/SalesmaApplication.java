ec5091c97e6b5c6822014f0ca216eb60f021b78d
ASARABIA <asarabias@unicesar.edu.co>
2022-03-17 10:37:37 -0500

    Agregando Archivos

---
+++ src/main/java/com/example/salesma/SalesmaApplication.java
@@ -0,0 +1,13 @@
+package com.example.salesma;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SalesmaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SalesmaApplication.class, args);
+	}
+
+}
