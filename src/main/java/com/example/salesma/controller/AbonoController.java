e205d50dd652940f0759e4388613b840848846fe
msgaray <90914937+msgaray@users.noreply.github.com>
2022-03-30 18:22:01 -0500

    metodos del CRUD

--- src/main/java/com/example/salesma/controller/AbonoController.java
+++ src/main/java/com/example/salesma/controller/AbonoController.java
@@ -1,9 +1,15 @@
 package com.example.salesma.controller;
 
+import java.util.ArrayList;
+import java.util.Optional;
+
 import com.example.salesma.model.AbonoModelo;
 import com.example.salesma.service.AbonoService;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -22,5 +28,19 @@ public class AbonoController {
         return service.SaveAbono(abonom);
     }
  
+    @GetMapping()
+    public ArrayList<AbonoModelo> GetAbono(){
+        return  service.GetAbono();
+    }
+    
+    @GetMapping(path="/{id}")
+    public Optional<AbonoModelo> GetAbonoId(@PathVariable("id") Long id){
+        return  service.GetAbonoId(id);
+    }
+    
+    @DeleteMapping(path="/{id}")
+    public void DeleteAbonoId(@PathVariable("id") Long id){
+          service.DeleteAbonoId(id);
+    }
     
 }
--- src/main/java/com/example/salesma/repository/RepoCliente.java
+++ src/main/java/com/example/salesma/repository/RepoCliente.java
@@ -5,13 +5,7 @@ import com.example.salesma.model.ClienteModelo;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
-<<<<<<< HEAD
 @Repository
 public interface RepoCliente extends MongoRepository<ClienteModelo, Long>{
-=======
-
-@Repository
-public interface RepoCliente extends MongoRepository<ClienteModelo,Long> {
->>>>>>> main_abraham
     
 }
