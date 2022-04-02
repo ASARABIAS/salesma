aab5cdaa3e0758010b48182e24353f08fef1986b
ASARABIAS <95699189+ASARABIAS@users.noreply.github.com>
2022-04-02 10:32:17 -0500

    Corrigiendo errores

--- src/main/java/com/example/salesma/controller/ProductController.java
+++ src/main/java/com/example/salesma/controller/ProductController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/producto")
-public class ProductoController {
+public class ProductController {
     @Autowired
     ProductoService productoService;
     
--- src/main/java/com/example/salesma/repository/PaymentRepository.java
+++ src/main/java/com/example/salesma/repository/PaymentRepository.java
@@ -1,11 +1,13 @@
 package com.example.salesma.repository;
 
-import com.example.salesma.model.AbonoModelo;
+
+
+import com.example.salesma.model.PaymentModel;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface RepoAbono extends MongoRepository<AbonoModelo,Long>{
+public interface PaymentRepository extends MongoRepository<PaymentModel,Long>{
     
 }
