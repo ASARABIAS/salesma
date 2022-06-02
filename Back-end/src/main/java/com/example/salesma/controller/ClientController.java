387833d743a0513652ee1498a6e2962cfdad7ba9
asarabias <asarabias@unicesar.edu.co>
2022-06-02 07:24:21 -0500

    realizando consultas a la api

--- Back-end/src/main/java/com/example/salesma/controller/ClientController.java
+++ Back-end/src/main/java/com/example/salesma/controller/ClientController.java
@@ -7,6 +7,7 @@ import com.example.salesma.model.ClientModel;
 import com.example.salesma.service.ClientService;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -14,9 +15,11 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin(origins = "*",methods = {RequestMethod.POST,RequestMethod.GET,RequestMethod.DELETE,RequestMethod.PUT})
 @RequestMapping("/client")
 public class ClientController {
 
